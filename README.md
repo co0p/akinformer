46eb9138218e6de8eb99a07297f812fa2a104f21
Julian Godesa <julian.godesa@googlemail.com>
2017-02-14 21:28:00 +0100

    draft: working towards fetching the data

--- README.md
+++ README.md
@@ -3,4 +3,10 @@ why don't you ASCK ?
 
 scraping the aerzte kammer job offer for fun and playing with google app engine
 
+Idea:
+-----
 
+Schedule a repeating scraping task. If the scraper examines the html anf if it finds a new entry
+on the job offer page, an email with the new offer(s) is being send.
+
+* cron.yaml - registers a periodic call to the service
--- asck.go
+++ asck.go
@@ -1,25 +1,66 @@
 package asck
 
 import (
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"time"
 
 	"google.golang.org/appengine"
-	"google.golang.org/appengine/user"
+	"google.golang.org/appengine/urlfetch"
 )
 
-// const OFFERS_URL "https://www.aerztekammer-berlin.de/10arzt/15_Weiterbildung/17WB-Stellenboerse/index.html"
+// Offer is a individual offer extracted from the job offer page
+type Offer struct {
+	Text      string
+	Contact   string
+	StartDate time.Time
+}
+
+var offersURL = "http://www.aerztekammer-berlin.de/10arzt/15_Weiterbildung/17WB-Stellenboerse/index.html"
 
 func init() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/sendNewJobOffers", sendNewJobOffers)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	c := appengine.NewContext(r)
-	name := "Anonymous"
-	if u := user.Current(c); u != nil {
-		name = u.String()
+	fmt.Fprintf(w, "Hello, world!")
+}
+
+func sendNewJobOffers(w http.ResponseWriter, r *http.Request) {
+
+	offers, err := extractJobOffers(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	fmt.Fprintf(w, "Hello, world!, %v", name)
+	log.Printf("extracted a few bits:%v", offers)
+
+	w.WriteHeader(http.StatusNoContent)
+	w.Write([]byte("204 - Send the job offers into the void"))
+}
+
+func extractJobOffers(r *http.Request) ([]Offer, error) {
+	ctx := appengine.NewContext(r)
+	client := urlfetch.Client(ctx)
+	resp, err := client.Get(offersURL)
+
+	if err != nil {
+		return nil, errors.New("Failed connecting to the offer page: " + err.Error())
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.New("Failed reading body from the response: " + err.Error())
+	}
+
+	if len(body) != 0 {
+		return nil, errors.New("No characters were read from the response: " + err.Error())
+	}
+
+	return []Offer{}, nil
 }
